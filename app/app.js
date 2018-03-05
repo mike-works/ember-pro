75880845d3a5ef84135188e7c9df17ce1416bbd5
Michael North <michael.l.north@gmail.com>
2018-03-05 12:18:53 -0800

    Exercise 1

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import { default as m, PI } from 'math';
+
+console.log(m.sqrt(4));
+console.log(PI);
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
