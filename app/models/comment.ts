71a9cb5b213d646aae72054128d777f253b391bc
Michael North <michael.l.north@gmail.com>
2018-03-08 12:07:51 -0800

    Exercise 12 starting point

--- app/models/comment.ts
+++ app/models/comment.ts
@@ -4,6 +4,7 @@ const { Model, attr, belongsTo } = DS;
 
 export default Model.extend({
   post: belongsTo('post'),
+  user: belongsTo('user'), // this.store.peekRecord('user', 1)
   body: attr('string'),
   createdAt: attr('string'),
   updatedAt: attr('string')
