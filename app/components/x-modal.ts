fc08fa68abe64af91cb0b2938f25478823ab4510
Mike North <michael.l.north@gmail.com>
2018-04-28 07:56:58 +0100

    Final

---
+++ app/components/x-modal.ts
@@ -0,0 +1,15 @@
+import Component from '@ember/component';
+
+export default class XModal extends Component.extend({
+  // anything which *must* be merged to prototype here
+}) {
+  isOpen = true
+  // normal class body definition here
+};
+
+
+
+[1, 2, 3].map((x, i) => {
+  console.log('item ', i);
+  return x * x;
+});
